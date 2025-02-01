a04d47ccdc2cf66c812aa8c9aee41b53b7050453
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-02-01 19:11:30 +0000

    Day 59 progress

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
+import requests
+from datetime import datetime
+
+response = requests.get('https://api.npoint.io/674f5423f73deab1e9a7').json()
 
 app = Flask(__name__)
-
+# title, subtitle, author and dates
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template('index.html', data= response, author="Api author", date=datetime.today().date())
 
 
+@app.route('/contact')
+def get_contact():
+  return render_template('contact.html', data= response, author="Api author", date=datetime.today().date())
+
+
+@app.route('/post')
+def get_post():
+  return render_template('post.html', data= response, author="Api author", date=datetime.today().date())
+
+
+@app.route('/about')
+def get_about():
+  return render_template('about.html', data= response, author="Api author", date=datetime.today().date())
+
+print(datetime.today().date())
+ 
 if __name__ == "__main__":
   app.run(debug=True)
