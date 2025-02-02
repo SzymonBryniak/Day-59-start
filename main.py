83244cf739d6c38e07d044743159ace3b31b3d8b
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-02-02 16:57:43 +0000

    Day 59 progress

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ def home():
 
 @app.route('/contact')
 def get_contact():
-  return render_template('contact.html', data= response, author="Api author", date=datetime.today().date())
+  return render_template('contact.html', data= response, author="Szymon Bryniak", date=datetime.today().date())
 
 
 @app.route('/post')
 def get_post():
-  return render_template('post.html', data= response, author="Api author", date=datetime.today().date())
+  return render_template('post.html', data= response, author="Szymon Bryniak", date=datetime.today().date().strftime('%B, %d, %Y'))
 
 
 @app.route('/about')
 def get_about():
   return render_template('about.html', data= response, author="Api author", date=datetime.today().date())
+
+
+@app.route('/ind_post/<int:id>')
+def goto_post(id):
+  return render_template('ind_post.html', data= response[id - 1], author="Api author", date=datetime.today().date().strftime('%B, %d, %Y'))
 
 print(datetime.today().date())
  
